c196ae93ac0df81d47327f4aa563815676b53442
pgku2001 <151089288+pgku2001@users.noreply.github.com>
2025-04-18 14:10:07 -0700

    Add files via upload
    Session 1 question 1 print name, pronouns etc

---
+++ Session 1 Prompt.py
@@ -0,0 +1,15 @@
+Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #This will print my name, my pronouns, and two sentences about my favorite movie and mt facvorite food.
+>>> print("Full name: Phillip")
+Full name: Phillip
+>>> print("Pronounds: He/Him")
+Pronounds: He/Him
+>>> print ("My favorite movie is 'Interstellar' because I am curious of Space and the movie makes me curious.")
+My favorite movie is 'Interstellar' because I am curious of Space and the movie makes me curious.
+>>> 
+============ RESTART: C:/Users/phill/OneDrive/Desktop/Session1q1.py ============
+Full Name: Phillip
+Pronouns: He/Him
+My favorite movie is 'Interstellar' because I am curious about space and the movie made me think about time differently.
+My favorite food is sushi.
